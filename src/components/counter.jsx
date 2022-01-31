06d3b56fc288e82888a6e84e27d04d82dcb5ce0a
Gopala <guptaraja727@gmail.com>
2022-02-01 00:05:42 +0530

    cc

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
+import { useState } from "react";
+import "./Counter.css"
 
-
-
-export const Counter = ()=>{
-    const [counter,setCounter] = useState(1)
-    const clicFun = (val)=>{
-      setCounter(counter + val)
-    }
-    return (
-        <div>
-            <h1>couner:{counter}</h1>
-            <button onClick={()=>clicFun(1)}>Increment</button>
-            <button onClick={()=>clicFun(-1)}>Decrement</button>
-        </div>
-    )
-}
+export const Counter = () => {
+  const [counter, setCounter] = useState(1);
+  const clicFun = (val) => {
+    setCounter(counter + val);
+  };
+  if (counter === 12) {
+    return <h1>You have reached to the maximun</h1>;
+  }
+  return (
+    <div>
+      <h1>Couner:{counter}</h1>
+      <h3 className={counter % 2 === 0 ? "red" : "green"}>
+        Number is:{counter % 2 === 0 ? "Even" : "Odd"}
+      </h3>
+      <button onClick={() => clicFun(1)}>Increment</button>
+      <button onClick={() => clicFun(-1)}>Decrement</button>
+    </div>
+  );
+};
