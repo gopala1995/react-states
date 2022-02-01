f80603b5cd8edf930354ca88cd04ecff628964dc
Gopala <guptaraja727@gmail.com>
2022-02-01 23:13:11 +0530

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Counter } from './components/counter';
-import { Userdeatails } from './userDetails';
+import { Userdeatails } from './components/userDetails';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
-      <Userdeatails name="Gopala" age="25"/>
+      <Counter/>
+      {/* <Userdeatails name="Gopala" age="25"/>
       <Userdeatails name="Raja" age="23"/>
-      <Userdeatails name="Aman" age="24"/>
+      <Userdeatails name="Aman" age="24"/> */}
     
     </div>
   );
--- src/components/userDetails.jsx
+++ src/components/userDetails.jsx
@@ -1,3 +1,5 @@
+
+
 export function Userdeatails(props){
     return (
         <div>
